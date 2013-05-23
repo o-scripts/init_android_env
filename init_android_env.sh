6b85d54a28f46f65c6dcdfffe6f7e0f14b654ee0
zgd_521 <zgd_521@126.com>
2013-05-23 22:19:21 +0800

    OverView: init_android_env BUG: None Desc: Add copyright for software Reviewer: None

--- init_android_env.sh
+++ init_android_env.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
+
 ########################################################################
-# Name: init_android_env
-# Version: v1.0
+#
 # Autor: M.R.Z
 # Email: zgd1348833@gmail.com
 # Date: @2013.05.23
-# This file is just used for build env for android develop in ubuntu
 #
+# Copyright (c) 2013 Computer Science, City Colleges, Xi'an Jiaotong University
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 ########################################################################
+
+VERSION="0.1"
+
 # Get arch Info
 arch_t=`uname -i`
 cd ~
